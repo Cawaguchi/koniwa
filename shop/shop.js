519ac51dfc41e6806012ae9dff6b5bda88999cf2
Kawat <c259007@ivy.ac.jp>
2025-12-14 00:47:36 +0900

    Add files via upload

--- shop/shop.js
+++ shop/shop.js
@@ -6,3 +6,18 @@ document.querySelector("footer a").addEventListener("click", (e) => {
         behavior: "smooth"
     });
 });
+
+
+/* ================================
+   Item 3 - 画像スライドショー
+================================ */
+const slides = document.querySelectorAll(".slide-wrapper img");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active"); // 今の画像を消す
+  index = (index + 1) % slides.length;      // 次へ
+  slides[index].classList.add("active");    // 次の画像表示
+}, 4000); // ←切り替え間隔（ゆっくり）4秒推奨
+
+
